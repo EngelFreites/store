329cb768c666bcb65c8a22e70c337aaa2a593da9
engelbert <engelfreites97@gmail.com>
2023-08-18 21:49:06 -0500

    feat: create header component

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header (){
+  return (  
+    <header>
+      <h1>Engel</h1>
+      <ul>
+        <li>Inicio</li>
+        <li>Hombre</li>
+        <li>Muejer</li>
+        <li>Niños</li>
+      </ul>
+      <div id='icons-header'>
+        <p>🛒</p>
+        <p>❤️</p>
+        <p>🚪</p>  
+      </div>
+    </header>
+  )
+
+}

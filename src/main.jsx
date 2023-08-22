8d6f1ba94d067abb9ee4aa1a4ffc34c4050e92c2
engelbert <engelfreites97@gmail.com>
2023-08-22 12:37:50 -0500

    refactor: agg global style to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '../src/assets/styles/variables.css'
+import '../src/assets/styles/bases.css'
+import '../src/assets/styles/reset.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(

751a42ec84681dff5c2bf2e38fd37b0a8ed59de8
engelbert <engelfreites97@gmail.com>
2023-08-23 11:56:32 -0500

    refactor: change to li's list  for a map

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,13 +1,17 @@
 import './header.css'
+
+const options = ['Camisas','Camisetas', 'Jeans', 'Pantalones', 'Bermudas', 'Accesorios']
+
 export default function Header (){
   return (  
     <header>
       <h1 className='text-title font-1 title-page'>ED</h1>
       <ul className='list-options'>
-        <li className='font-2 option'>Inicio</li>
-        <li className='font-2 option'>Hombre</li>
-        <li className='font-2 option'>Muejer</li>
-        <li className='font-2 option'>Niños</li>
+        {
+          options.map( option =>(
+            <li key={option} className='font-2 option'>{option}</li>
+          ))
+        }
       </ul>
       <div id='icons-header'>
         <p>🛒</p>
